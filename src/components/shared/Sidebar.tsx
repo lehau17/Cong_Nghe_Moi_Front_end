776b320753857b5521e49fbd5903c2a5536b7c95
lehau17 <haule@hitek.com.vn>
2025-03-25 16:19:53 +0700

    FIX: fix sidebar with dropdown menu

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
 import { FaBriefcase, FaCloud, FaUserFriends } from "react-icons/fa";
 import { HiOutlineClipboardList } from "react-icons/hi";
 import { IoChatbubbleEllipsesSharp, IoSettingsSharp } from "react-icons/io5";
@@ -5,10 +7,19 @@ import { IoChatbubbleEllipsesSharp, IoSettingsSharp } from "react-icons/io5";
 const Sidebar = () => {
   return (
     <div className="w-16 bg-blue-600 h-screen flex flex-col items-center py-4 space-y-6 text-white">
-      {/* Avatar */}
-      <div className="w-10 h-10 bg-white text-blue-600 flex items-center justify-center rounded-full font-bold">
-        LH
-      </div>
+      {/* Avatar + Dropdown Menu */}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-10 h-10 bg-white text-blue-600 flex items-center justify-center rounded-full font-bold cursor-pointer">
+          LH
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 bg-white text-black shadow-lg p-2" side="right">
+                  <h3 className= "font-semibold p-2 border-b">Lê Trung Hậu</h3>
+                  <DropdownMenuSeparator />
+          <DropdownMenuItem>Hồ sơ của bạn</DropdownMenuItem>
+          <DropdownMenuItem>Cài đặt</DropdownMenuItem>
+          <DropdownMenuItem className="text-red-500">Đăng xuất</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       {/* Menu Icons */}
       <div className="flex flex-col space-y-6">
