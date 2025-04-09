58d36f6b3dc53575a088641f9d829f1a1d2789ef
haule@hitek.com.vn <phamhuyentrang562003@gmail.com>
2025-04-09 17:54:29 +0700

    FEA : update profile

--- src/components/shared/ProfileModal.tsx
+++ src/components/shared/ProfileModal.tsx
@@ -1,13 +1,12 @@
 import { getUserProfile } from "@/apis/user.api";
-import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { useUploadAvatar } from "@/queries/upload.query";
+import { CaretLeftOutlined } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
-import { Camera, Pencil } from "lucide-react";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+import EditProfileForm from "./EditProfileForm";
+import ProfileView from "./ProfileView";
 const ProfileModal = ({ open, setOpen }: { open: boolean; setOpen: any }) => {
-    const { data, isLoading, error, refetch } = useQuery({
+    const { data, refetch } = useQuery({
         queryKey: ["userProfile"],
         queryFn: getUserProfile,
         enabled: open, // chỉ fetch khi open
@@ -22,34 +21,22 @@ const ProfileModal = ({ open, setOpen }: { open: boolean; setOpen: any }) => {
     const profile = data?.data?.data;
 
 
-    const { mutate: uploadAvatar, isPending, data: dataUpload } = useUploadAvatar();
 
-    const handleAvatarUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (!file) return;
-
-        uploadAvatar(file, {
-            onSuccess: () => {
-                refetch()
-            },
-
-            onError: (err) => {
-                console.error("Upload error:", err);
-            },
-        });
-    };
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent className="max-w-lg p-0 rounded-sm">
                 <DialogHeader className="relative">
                     <DialogTitle className="p-4 text-lg font-semibold">
-                        {isEditing ? "Cập nhật thông tin cá nhân" : "Thông tin tài khoản"}
+                        {isEditing ? <div onClick={() => setIsEditing(false)} className="absolute left-4 top-4">
+                            <CaretLeftOutlined className="cursor-pointer" />
+                            <span>Cập nhật thông tin tài khoản</span>
+                        </div> : "Thông tin tài khoản"}
                     </DialogTitle>
                 </DialogHeader>
 
                 {isEditing ? (
-                    <EditProfileForm profile={profile} onBack={() => setIsEditing(false)} />
+                    <EditProfileForm profile={profile} onBack={() => setIsEditing(false)} setIsEdit={setIsEditing} />
                 ) : (
                     <ProfileView profile={profile} onEdit={() => setIsEditing(true)} />
                 )}
