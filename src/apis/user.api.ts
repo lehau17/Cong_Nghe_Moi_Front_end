e2beaad9970ab79bf3d3c99574c99be4e40c2f21
haule@hitek.com.vn <phamhuyentrang562003@gmail.com>
2025-05-27 07:34:56 +0700

    fix url

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -4,7 +4,7 @@ import { SuccessResponse } from "@/types/utils.type";
 
 
 export const getUserProfile = () => http.get<SuccessResponse<UserProfile>>("/user/me");
-export const updateUserProfile = (data: UserUpdateProfile) => http.patch<SuccessResponse<UserProfile>>("/   user/me", data);
+export const updateUserProfile = (data: UserUpdateProfile) => http.patch<SuccessResponse<UserProfile>>("/user/me", data);
 
 export const searchUserByPhone = async (phone: string) => {
     const res = await http.get<SuccessResponse<UserProfile & { relationship: string, isSender: boolean, rs_id: string }>>(`/user/search?phone=${phone}`);
